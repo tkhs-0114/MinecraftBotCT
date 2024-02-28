213774c41dbb18a7f66200ace1a104f3aea7b0fa
tkhs-0114 <135799380+tkhs-0114@users.noreply.github.com>
2024-02-29 00:46:16 +0900

    zonbi_2 OK

--- codebot.c
+++ codebot.c
@@ -4,6 +4,7 @@
 
 int main(char *argv){
     int han1;
+    int P1, P2, P3, P4, P5; 
     long han2;
     int han3;
     init();
@@ -16,26 +17,39 @@ int main(char *argv){
         //printf("han1=%d\n" , han1);
         //printf("han2=%ld\n" , han2);
 
-        if((han1 / 1000000)%10 == 1){
-            printf("|左上|");
-        }else{printf("|　　|");}
-        if((han1 / 100000)%10 == 1){
-            printf("中上|");
-        }else{printf("　　|");}
-        if((han1 / 10000)%10 == 1){
-            printf("右上|");
-        }else{printf("　　|");}
+        P1 = (han2 / 1000000000000) % 1000;
+        P2 = (han2 / 1000000000) % 1000;
+        P3 = (han2 / 1000000) % 1000;
+        P4 = (han2 / 1000) % 1000;
+        P5 = (han2 / 1) % 1000;
+
+        if((P4/1)%10){printf("近");}
+        else if((P4/10)%10){printf("中");}
+        else if((P4/100)%10){printf("遠");}
+        else{printf("　");}
+        printf("|　|");
+        if((P3/1)%10){printf("近");}
+        else if((P3/10)%10){printf("中");}
+        else if((P3/100)%10){printf("遠");}
+        else{printf("　");}
         printf("\n");
-        if((han1 / 1000)%10 == 1){
-            printf("|左下|");
-        }else{printf("|　　|");}
-        if((han1 / 100)%10 == 1){
-            printf("中下|");
-        }else{printf("　　|");}
-        if((han1 / 10)%10 == 1){
-            printf("右下|");
-        }else{printf("　　|");}
-        printf("\n---------------\n");
+        if((P2/1)%10){printf("近");}
+        else if((P2/10)%10){printf("中");}
+        else if((P2/100)%10){printf("遠");}
+        else{printf("　");}
+        printf("|");
+        if((P5/1)%10){printf("近");}
+        else if((P5/10)%10){printf("中");}
+        else if((P5/100)%10){printf("遠");}
+        else{printf("　");}
+        printf("|");
+        if((P1/1)%10){printf("近");}
+        else if((P1/10)%10){printf("中");}
+        else if((P1/100)%10){printf("遠");}
+        else{printf("　");}
+        printf("\n------------\n");
+
+        //printf("\n---------------\n");
 
         sleep(1);
     }
