14e076a56acbe9ad3cee3d2fdd93e0b47dad4e1f
tkhs-0114 <135799380+tkhs-0114@users.noreply.github.com>
2024-02-29 01:13:09 +0900

    zonbi_3 OK

--- codebot.c
+++ codebot.c
@@ -14,42 +14,17 @@ int main(char *argv){
     while(rk){//ここにプログラム
         han1 = detectZombie1();
         han2 = detectZombie2();
+        han3 = detectZombie3();
         //printf("han1=%d\n" , han1);
         //printf("han2=%ld\n" , han2);
 
-        P1 = (han2 / 1000000000000) % 1000;
-        P2 = (han2 / 1000000000) % 1000;
-        P3 = (han2 / 1000000) % 1000;
-        P4 = (han2 / 1000) % 1000;
-        P5 = (han2 / 1) % 1000;
-
-        if((P4/1)%10){printf("近");}
-        else if((P4/10)%10){printf("中");}
-        else if((P4/100)%10){printf("遠");}
-        else{printf("　");}
-        printf("|　|");
-        if((P3/1)%10){printf("近");}
-        else if((P3/10)%10){printf("中");}
-        else if((P3/100)%10){printf("遠");}
-        else{printf("　");}
-        printf("\n");
-        if((P2/1)%10){printf("近");}
-        else if((P2/10)%10){printf("中");}
-        else if((P2/100)%10){printf("遠");}
-        else{printf("　");}
-        printf("|");
-        if((P5/1)%10){printf("近");}
-        else if((P5/10)%10){printf("中");}
-        else if((P5/100)%10){printf("遠");}
-        else{printf("　");}
-        printf("|");
-        if((P1/1)%10){printf("近");}
-        else if((P1/10)%10){printf("中");}
-        else if((P1/100)%10){printf("遠");}
-        else{printf("　");}
-        printf("\n------------\n");
-
-        //printf("\n---------------\n");
+        if((han3/1)%10){printf("〇");}else{printf("　");}
+        if((han3/10)%10){printf("〇");}else{printf("　");}
+        if((han3/100)%10){printf("〇");}else{printf("　");}
+        if((han3/1000)%10){printf("〇");}else{printf("　");}
+        if((han3/10000)%10){printf("〇");}else{printf("　");}
+        if((han3/100000)%10){printf("〇");}else{printf("　");}
+        printf("\n---------------\n");
 
         sleep(1);
     }
