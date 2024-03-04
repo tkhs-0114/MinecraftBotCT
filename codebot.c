1cdcff1aaaecec86bc4958b69b5684d59b16453d
tkhs-0114 <135799380+tkhs-0114@users.noreply.github.com>
2024-03-04 23:26:22 +0900

    look zonbi

--- codebot.c
+++ codebot.c
@@ -2,32 +2,75 @@
 #include <unistd.h>
 #include "control.h"
 
+int ZONBI_F(int han){
+    if((han/100)%10 == 1){
+        printf("FRONT\n");
+        return 0;
+    }
+    if((han/100000)%10 == 1){
+        printf("LONG\n");
+        return 0;
+    }
+    return 1;
+}
+int ZONBI_L(int han){
+    if((han/1000000)%10 == 1){
+        return 0;
+    }
+    if((han/1000)%10 == 1){
+        return 0;
+    }
+    return 1;
+}
+int ZONBI_R(int han){
+    if((han/10000)%10 == 1){
+        return 0;
+    }
+    if((han/10)%10 == 1){
+        return 0;
+    }
+    return 1;
+}
+
 int main(char *argv){
     int han1;
-    int P1, P2, P3, P4, P5; 
     long han2;
-    int han3;
+    int cnt = 0;
+    int Rote = 0;
     init();
     setTime();
     exePython();
     //setSurvival();
     while(rk){//ここにプログラム
         han1 = detectZombie1();
-        han2 = detectZombie2();
-        han3 = detectZombie3();
+        //han2 = detectZombie2();
         //printf("han1=%d\n" , han1);
         //printf("han2=%ld\n" , han2);
 
-        if((han3/1)%10){printf("〇");}else{printf("　");}
-        if((han3/10)%10){printf("〇");}else{printf("　");}
-        if((han3/100)%10){printf("〇");}else{printf("　");}
-        if((han3/1000)%10){printf("〇");}else{printf("　");}
-        if((han3/10000)%10){printf("〇");}else{printf("　");}
-        if((han3/100000)%10){printf("〇");}else{printf("　");}
-        printf("\n---------------\n");
+        cnt++;
 
+        han1 = detectZombie1();
+        if(ZONBI_L(han1) == 0){Rote=1;}else{Rote=-1;}
+
+        while(ZONBI_F(han1)){
+            if(!rk){break;};
+            if(Rote==1){cameraLeft(0.4);}
+            else if(Rote==-1){cameraRight(0.4);}
+            han1 = detectZombie1();
+        }
         sleep(1);
+
+        
+        //cameraRight(0.4);
+        //moveJump(2);
+        //cameraLeft(0.4);
+
+        
+
+        printf("%d\n", cnt%10);
+
+        //sleep(0.1);
     }
-    setCreative();
-    setMorning();
+    //setCreative();
+    //setMorning();
 }
